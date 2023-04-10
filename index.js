ddbd0aebef4001a25810b5fc30949943d2cd286f
unknown <toropov.nikita2004@gmail.com>
2023-04-10 10:58:21 +0200

    feat: send file information to the user

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var cors = require('cors');
+const multer = require("multer");
 require('dotenv').config()
 
 var app = express();
@@ -11,6 +12,10 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post("/api/fileanalyse", multer().any(),(req, res) => {
+  const {originalname, mimetype, size} = req.files[0];
+  res.json({"name": originalname, "type": mimetype, "size": size});
+})
 
 
 
